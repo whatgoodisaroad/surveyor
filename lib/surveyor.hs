bf9419e9e6f317e0fa6b6ea495fa2b896276fe1a
Wyatt Allen <wyatt.allen0@gmail.com>
2012-04-24 09:59:48 -0700

    Updated type system

--- lib/surveyor.hs
+++ lib/surveyor.hs
@@ -1,110 +1,144 @@
 
-
--- DSL
+{-#LANGUAGE GADTs #-}
 
 type Prompt = String
 
-data Question = 
-      MultipleChoice Prompt [Choice] 
-    | FreeResponse Prompt
+data Survey a where
+    FreeResponse :: Prompt -> Survey String
+    MultipleChoice :: Prompt -> Choice a -> Survey a
+    (:-:) :: Survey b -> Survey c -> Survey (b, c)
 
-data Choice = 
-      Choice String
-    | ChoicePlus String Survey
+data Choice a where
+    Option :: String -> a -> Choice a
+    OptionPlus :: String -> a -> Survey b -> Choice (a, b)
+    (:+:) :: Choice a -> Choice a -> Choice a
+    (:*:) :: Choice b -> Choice c -> Choice (Either b c)
+
+-- String option combinator
+
+stringOption :: String -> Choice String
+stringOption text = Option text text
     
-data Survey =
-      Pose Question
-    | Survey :-: Survey
 
-type Answer = (Prompt, String)
+-- Example:
 
--- Example
-    
-simpleSurvey :: Survey
+simpleSurvey :: Survey (String, String)
 simpleSurvey = 
-        (Pose $ FreeResponse "First name")
-    :-: (Pose $ FreeResponse "Last name")
-    
-    
-    
-mySurvey :: Survey
-mySurvey = 
-        (Pose $ FreeResponse "First name")
-    :-: (Pose $ FreeResponse "Last name")
-    :-: (Pose $ MultipleChoice "Age group" [
-            Choice "< 20",
-            Choice "21 - 40",
-            Choice "41 - 60",
-            Choice "> 60"
-            ]
+        FreeResponse "First name"
+    :-: FreeResponse "Last name"
+
+mySurvey :: Survey 
+    (
+        (
+            (String, String), 
+            String
+        ), 
+        Either 
+            String 
+            (
+                String, 
+                (String, String)
+            )
+   ) 
+
+mySurvey =
+        simpleSurvey
+    :-: MultipleChoice "Age group" (
+                stringOption "< 20" 
+            :+: stringOption "21 - 40"
+            :+: stringOption "41 - 60"
+            :+: stringOption "> 60"
         )
-    :-: (Pose $ MultipleChoice "Occupation" [
-            Choice "Retail",
-            Choice "Education",
-            ChoicePlus "Government" govtQuestions
-            ]
+    :-: MultipleChoice "Occupation" (
+                stringOption "Retail"
+            :+: stringOption "Education"
+            :*: OptionPlus "Government" "Government" govtQuestions
         )
     where
-        govtQuestions :: Survey
-        govtQuestions = 
-                (Pose $ MultipleChoice "Department" [
-                    Choice "Federal",
-                    Choice "Municipal"
-                ])
-            :-: (Pose $ FreeResponse "Boss' last name")
-        
+        govtQuestions :: Survey (String, String)
+        govtQuestions =
+                MultipleChoice "Department" (
+                        stringOption "Federal"
+                    :+: stringOption "Municipal"
+                )
+            :-: FreeResponse "Boss' last name"
 
-    
+-- Lickert question combinator
 
+data LikertScale = 
+      StronglyAgree
+    | Agree
+    | NeitherNor
+    | Disagree
+    | StronglyDisagree
+    deriving (Show)
 
-
-
-
-
-
-
-
+likert :: Prompt -> Survey LikertScale
+likert prompt = MultipleChoice prompt (
+            Option "Strongly disagree"          StronglyDisagree
+        :+: Option "Disagree"                   Disagree
+        :+: Option "Neither agree nor disagree" NeitherNor
+        :+: Option "Agree"                      Agree
+        :+: Option "Strongly agree"             StronglyAgree
+    )
 
 -- Run
 
-surveyCli :: Survey -> IO [Answer]
-surveyCli (Pose q) = do
-    ans <- questionToPrompt q
-    return ans
-surveyCli (q1 :-: q2) = do
-    as1 <- surveyCli q1
-    as2 <- surveyCli q2
-    return $ as1 ++ as2
-    
-questionToPrompt :: Question -> IO [Answer]
-questionToPrompt (FreeResponse prompt) = do
+selected :: Int -> Choice a -> IO a
+selected 0 (Option _ val) = return val
+selected 0 (OptionPlus _ val sub) = do
+    subval <- surveyCli sub
+    return (val, subval)
+selected n (l :+: r) = do
+    let llen = choiceLength l
+    if n < llen
+        then selected n l
+        else selected (n - llen) r
+selected n (l :*: r) = do
+    let llen = choiceLength l
+    if n < llen 
+        then fmap Left $ selected n l
+        else fmap Right $ selected (n - llen) r
+
+choiceLength :: Choice a -> Int
+choiceLength (Option _ _) = 1
+choiceLength (OptionPlus _ _ _) = 1
+choiceLength (l :+: r) = choiceLength l + choiceLength r
+choiceLength (l :*: r) = choiceLength l + choiceLength r
+
+surveyCli :: Survey a -> IO a
+ 
+surveyCli (FreeResponse prompt) = do
     putStr $ "\n" ++ prompt ++ ": "
     ans <- getLine
-    return [(prompt, ans)]
-questionToPrompt (MultipleChoice prompt choices) = do
-    putStrLn $ "\n" ++ prompt ++ ":"
-    dispChoices 1 choices
+    return ans
+
+surveyCli (MultipleChoice prompt choiceExp) = do
+    putStr $ "\n" ++ prompt ++ ": \n"
+    dispChoices 1 choiceExp
     ans <- readLn :: IO Int
-    
-    let selected = choices !! pred ans
-    
-    plus <- case selected of
-        (Choice _) -> return []
-        (ChoicePlus _ sub) -> surveyCli sub
-    
-    return $ [(prompt, promptOf selected)] ++ plus
-    where
-        dispChoices :: Int -> [Choice] -> IO ()
-        dispChoices n [] = return ()
-        dispChoices n ((Choice text):cs) = do
-            putStrLn $ "[" ++ show n ++ "] " ++ text
-            dispChoices (n + 1) cs
-        dispChoices n ((ChoicePlus text _):cs) = do
-            putStrLn $ "[" ++ show n ++ "] " ++ text
-            dispChoices (n + 1) cs
-        promptOf :: Choice -> String
-        promptOf (Choice p) = p
-        promptOf (ChoicePlus p _) = p
+    selected (ans - 1) choiceExp
+        where
+            dispChoices :: Int -> Choice a -> IO Int
+            dispChoices num (Option text val) = do
+                putStrLn $ "[" ++ show num ++ "] " ++ text
+                return $ num + 1
+            dispChoices num (OptionPlus text val _) = do
+                putStrLn $ "[" ++ show num ++ "] " ++ text
+                return $ num + 1
+            dispChoices num (l :+: r) = do
+                next <- dispChoices num l
+                dispChoices next r
+            dispChoices num (l :*: r) = do
+                next <- dispChoices num l
+                dispChoices next r
+
+surveyCli (left :-: right) = do
+    la <- surveyCli left
+    ra <- surveyCli right
+    return (la, ra)
+
+
 
 
 
